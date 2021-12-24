080ba509376093fc94ece3e110ec4387d6adf940
Juun Pak <june.mintlatte@gmail.com>
2021-12-24 19:13:30 +0900

    ✨ Add a task state and input handler at Header

--- todoMVC-react-ts/src/components/Header.tsx
+++ todoMVC-react-ts/src/components/Header.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Header = () => (
-  <header className="header">
-    <h1>todos</h1>
-    <input className="new-todo" placeholder="What needs to be done?" value="" />
-  </header>
-);
+const Header = () => {
+  const [task, setTask] = useState('');
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
+
+  const onInputKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') setTask('');
+  };
+
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={task}
+        onChange={onInputChange}
+        onKeyUp={onInputKeyUp}
+      />
+    </header>
+  );
+};
 
 export default Header;
