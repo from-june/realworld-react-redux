961db2c6b8cc25d89b621be60c7ac49d7879d160
Juun Pak <june.mintlatte@gmail.com>
2021-12-24 18:57:36 +0900

    📝 Set an initial state to todoList state

--- todoMVC-react-ts/src/components/Main.tsx
+++ todoMVC-react-ts/src/components/Main.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import TodoItem from 'components/TodoItem';
 
-const Main = () => (
-  <main className="main">
-    <input
-      id="toggle-all"
-      className="toggle-all"
-      type="checkbox"
-      checked={false}
-    />
-    <label htmlFor="toggle-all"></label>
-    <ul className="todo-list">
-      <TodoItem task={'To do an assignment'} completed={false} />
-    </ul>
-  </main>
-);
+const Main = () => {
+  const initialState = [
+    { id: uuid(), task: 'To do an assignment', completed: false },
+    { id: uuid(), task: 'Be a FE developer', completed: true },
+    { id: uuid(), task: 'Do what I do like', completed: true }
+  ];
+
+  const [todoList, setTodoList] = useState(initialState);
+
+  return (
+    <main className="main">
+      <input
+        id="toggle-all"
+        className="toggle-all"
+        type="checkbox"
+        checked={false}
+      />
+      <label htmlFor="toggle-all"></label>
+      <ul className="todo-list">
+        {todoList.map(todo => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            task={todo.task}
+            completed={todo.completed}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
 
 export default Main;
--- todoMVC-react-ts/src/components/TodoItem.tsx
+++ todoMVC-react-ts/src/components/TodoItem.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
 type Todo = {
+  id: string;
   task: string;
   completed: boolean;
 };
 
-const TodoItem = ({ task, completed }: Todo) => (
-  <li className={completed ? 'completed' : ''}>
-    <div className="view">
-      <input className="toggle" type="checkbox" checked={completed} />
-      <label>{task}</label>
-      <button className="destroy"></button>
-    </div>
-    <input className="edit" value="투두 리스트" />
-  </li>
-);
+const TodoItem = ({ id, task, completed }: Todo) => {
+  return (
+    <li className={completed ? 'completed' : ''} data-task-id={id}>
+      <div className="view">
+        <input className="toggle" type="checkbox" checked={completed} />
+        <label>{task}</label>
+        <button className="destroy"></button>
+      </div>
+      <input className="edit" value="투두 리스트" />
+    </li>
+  );
+};
 
 export default TodoItem;
