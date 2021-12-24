a55e7d0ae58c61fe9dee8eb415dae7e84354a8cf
Juun Pak <june.mintlatte@gmail.com>
2021-12-24 20:39:51 +0900

    🔥 Remove the initial state and add a comment

--- todoMVC-react-ts/src/components/App.tsx
+++ todoMVC-react-ts/src/components/App.tsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
 import Header from 'components/Header';
 import Main from 'components/Main';
 import Footer from 'components/Footer';
 
 const App = () => {
-  const initialState = [
-    { id: uuid(), task: 'To do an assignment', completed: false },
-    { id: uuid(), task: 'Be a FE developer', completed: true },
-    { id: uuid(), task: 'Do what I do like', completed: true }
-  ];
-
-  const [todoList, setTodoList] = useState(initialState);
+  const [todoList, setTodoList] = useState([]);
 
   return (
     <section className="todoapp">
       <>
-        <Header addTask={setTodoList} />
+        <Header addTask={setTodoList} /> {/*FIXME: Need to fix a type*/}
         <Main todoList={todoList} />
         <Footer />
       </>
