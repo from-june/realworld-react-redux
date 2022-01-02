f70cb64ec24acd2844731e6433e7ed3f808f0207
Juun Pak <june.mintlatte@gmail.com>
2022-01-02 21:45:47 +0900

    🎨 Set action types

---
+++ todoMVC-react-ts/src/modules/actions/actions.ts
@@ -0,0 +1,70 @@
+import { v4 as uuid } from 'uuid';
+import {
+  LOAD_TODO_LIST,
+  ADD_TASK,
+  DELETE_TASK,
+  TOGGLE_TASK,
+  MODIFY_TASK,
+  TOGGLE_ALL_TASKS,
+  CLEAR_COMPLETED
+} from 'modules/actions/types';
+
+const loadTodoList = (state: ITodo[]) => {
+  return {
+    type: LOAD_TODO_LIST,
+    payload: state
+  };
+};
+
+const addTask = (task: string) => {
+  if (task === '') return;
+
+  const newTask = { task, completed: false, id: uuid() };
+
+  return {
+    type: ADD_TASK,
+    payload: newTask
+  };
+};
+
+const deleteTask = (taskId: string) => {
+  return {
+    type: DELETE_TASK,
+    payload: { id: taskId }
+  };
+};
+
+const toggleTask = (taskId: string) => {
+  return {
+    type: TOGGLE_TASK,
+    payload: { id: taskId }
+  };
+};
+
+const modifyTask = (taskId: string, newTask: string) => {
+  return {
+    type: MODIFY_TASK,
+    payload: {
+      id: taskId,
+      task: newTask
+    }
+  };
+};
+
+const toggleAllTasks = {
+  type: TOGGLE_ALL_TASKS
+};
+
+const clearCompleted = {
+  type: CLEAR_COMPLETED
+};
+
+export {
+  loadTodoList,
+  addTask,
+  modifyTask,
+  deleteTask,
+  toggleTask,
+  toggleAllTasks,
+  clearCompleted
+};
--- todoMVC-react-ts/src/modules/reducers.ts
+++ todoMVC-react-ts/src/modules/reducers.ts
@@ -1,4 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import {
+  LOAD_TODO_LIST,
+  ADD_TASK,
+  DELETE_TASK,
+  TOGGLE_TASK,
+  MODIFY_TASK,
+  TOGGLE_ALL_TASKS,
+  CLEAR_COMPLETED
+} from 'modules/actions/types';
 
 const initialState: ITodo[] = [
   {
@@ -24,34 +33,34 @@ const todoListReducer = (
       }
 ) => {
   switch (action.type) {
-    case 'LOAD_TODO_LIST':
+    case LOAD_TODO_LIST:
       return [...action.payload];
 
-    case 'ADD_TASK':
+    case ADD_TASK:
       return [action.payload, ...state];
 
-    case 'DELETE_TASK':
+    case DELETE_TASK:
       return state.filter(todo => todo.id !== action.payload.id);
 
-    case 'TOGGLE_TASK':
+    case TOGGLE_TASK:
       return state.map(todo =>
         todo.id === action.payload.id
           ? { ...todo, completed: !todo.completed }
           : todo
       );
 
-    case 'MODIFY_TASK':
+    case MODIFY_TASK:
       return state.map(todo =>
         todo.id === action.payload.id
           ? { ...todo, task: action.payload.task }
           : todo
       );
 
-    case 'TOGGLE_ALL_TASKS':
+    case TOGGLE_ALL_TASKS:
       const isCompletedAll = state.every(todo => todo.completed);
       return state.map(todo => ({ ...todo, completed: !isCompletedAll }));
 
-    case 'CLEAR_COMPLETED':
+    case CLEAR_COMPLETED:
       return state.filter(todo => todo.completed === false);
 
     default:
