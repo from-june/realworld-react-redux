74c46b7bc655fb90d73d0e5c7f346dc41c86ac4e
Juun Pak <june.mintlatte@gmail.com>
2022-01-02 21:29:06 +0900

    ⚡️ Provide the store to app

--- todoMVC-react-ts/src/index.tsx
+++ todoMVC-react-ts/src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'modules/store';
 import App from 'components/App';
 
 import 'style.css';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#app')
+);
