a2144bc2d9f1ee0fbda07aa2674a75cb311f27a4
Juun Pak <june.mintlatte@gmail.com>
2022-01-06 13:12:56 +0900

    🔥 Remove Provider and redux store

--- todoMVC-react-ts/src/index.tsx
+++ todoMVC-react-ts/src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from 'modules/store';
 import App from 'components/App';
 
 import 'style.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#app')
-);
+ReactDOM.render(<App />, document.querySelector('#app'));
